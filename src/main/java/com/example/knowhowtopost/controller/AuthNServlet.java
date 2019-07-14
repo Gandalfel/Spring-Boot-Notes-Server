cee7e320ec265a9ead6ca2b1ced54269af116795
kubaice3@gmail.com <kubaice3@gmail.com>
2019-07-14 22:53:17 +0200

    Validation fixed (max length).

--- src/main/java/com/example/knowhowtopost/controller/AuthNServlet.java
+++ src/main/java/com/example/knowhowtopost/controller/AuthNServlet.java
@@ -1,79 +1,26 @@
 package com.example.knowhowtopost.controller;
 
 import com.example.knowhowtopost.model.UserDto;
-import com.example.knowhowtopost.exception.InternalServerError;
-import com.example.knowhowtopost.repository.UserRepository;
-import com.example.knowhowtopost.model.UserEntity;
 
+import com.example.knowhowtopost.service.AuthNService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 @RestController
 @RequestMapping(path = "/REST/v1/AuthN")
 public class AuthNServlet {
 
     @Autowired
-    private UserRepository userRepository;
-
-    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
-            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-
-    public static boolean validateEmail(String emailStr) {
-        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
-        return matcher.find();
-    }
+    private AuthNService authNService;
 
     @PostMapping(path = "/signUp")
     public ResponseEntity<String> signIn(@RequestBody UserDto userDto) {
 
-        UserEntity userEntityByLogin = userRepository.findUserEntityByLogin(userDto.getLogin());
-        UserEntity userEntityByEmail = userRepository.findUserEntityByEmail(userDto.getEmail());
-
-        if (userEntityByLogin != null) {
-
-            throw new InternalServerError("Chosen login isn't free. Choose another one.");
-
-        } else if (userEntityByEmail != null) {
-
-            throw new InternalServerError("Email is already used.");
-
-        } else if (userDto.getLogin().length() <= 6) {
-
-            throw new InternalServerError("Chosen login is too short or too long. Login length: 5-15.");
-
-        } else if (userDto.getPassword().length() <= 9 || userDto.getPassword().length() >= 31) {
-
-            throw new InternalServerError("Chosen password is too short or too long. Password length: 8-30.");
-
-        } else if (!validateEmail(userDto.getEmail())) {
-
-            throw new InternalServerError("Bad email format.");
-
-        } else {
-
-            StringBuilder response = new StringBuilder();
-
-            UserEntity user = new UserEntity();
-            user.setLogin(userDto.getLogin());
-            user.setPassword(userDto.getPassword());
-            user.setEmail(userDto.getEmail());
-            userRepository.save(user);
-
-            for (UserEntity u : userRepository.findAll()) {
-                response.append(u.toString()).append("\n");
-            }
-
-            return new ResponseEntity<>(response.toString(), HttpStatus.OK);
-
-        }
+        return authNService.userDataValidation(userDto);
 
     }
 
--- src/main/java/com/example/knowhowtopost/service/AuthNService.java
+++ src/main/java/com/example/knowhowtopost/service/AuthNService.java
@@ -1,9 +1,74 @@
 package com.example.knowhowtopost.service;
 
+import com.example.knowhowtopost.exception.InternalServerError;
+import com.example.knowhowtopost.model.UserDto;
+import com.example.knowhowtopost.model.UserEntity;
+import com.example.knowhowtopost.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class AuthNService {
 
+    private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    private boolean validateEmail(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+        return matcher.find();
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public ResponseEntity<String> userDataValidation(UserDto userDto) {
+
+        UserEntity userEntityByLogin = userRepository.findUserEntityByLogin(userDto.getLogin());
+        UserEntity userEntityByEmail = userRepository.findUserEntityByEmail(userDto.getEmail());
+
+        if (userEntityByLogin != null) {
+
+            throw new InternalServerError("Chosen login isn't free. Choose another one.");
+
+        } else if (userEntityByEmail != null) {
+
+            throw new InternalServerError("Email is already used.");
+
+        } else if (userDto.getLogin().length() <= 4) {
+
+            throw new InternalServerError("Chosen login is too short or too long. Login length: 5-15.");
+
+        } else if (userDto.getPassword().length() <= 7 || userDto.getPassword().length() >= 31) {
+
+            throw new InternalServerError("Chosen password is too short or too long. Password length: 8-30.");
+
+        } else if (!validateEmail(userDto.getEmail())) {
+
+            throw new InternalServerError("Bad email format.");
+
+        } else {
+
+            StringBuilder response = new StringBuilder();
+
+            UserEntity user = new UserEntity();
+            user.setLogin(userDto.getLogin());
+            user.setPassword(userDto.getPassword());
+            user.setEmail(userDto.getEmail());
+            userRepository.save(user);
+
+            for (UserEntity u : userRepository.findAll()) {
+                response.append(u.toString()).append("\n");
+            }
+
+            return new ResponseEntity<>(response.toString(), HttpStatus.OK);
+
+        }
+
+    }
 
 }
